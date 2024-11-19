c763e8fe1f4b24a69708766b9c64f0e0055274be
ZhangYantao <zhangyantao@rivergame.net>
2024-11-19 16:45:16 +0800

    feat: 迁移env 的申明到子包下

--- apps/risk-control-antd/src/api/rg-request.ts
+++ apps/risk-control-antd/src/api/rg-request.ts
@@ -197,6 +197,7 @@ function createRequestClient(baseURL: string) {
   // 通用的错误处理,如果没有进入上面的错误处理逻辑，就会进入这里
   client.addResponseInterceptor(
     errorMessageResponseInterceptor(async (vMsg: string, error) => {
+      console.error('请求错误', vMsg);
       // 这里可以根据业务进行定制,你可以拿到 error 内的信息进行定制化处理，根据不同的 code 做不同的提示，而不是直接使用 message.error 提示 msg
       // 当前mock接口返回的错误字段是 error 或者 message
       const authStore = useAuthStore();
--- apps/risk-control-antd/src/types/vite-env.d.ts
+++ apps/risk-control-antd/src/types/vite-env.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="vite/client" />
+// / <reference types="vite/client" />
 
 export interface ImportMetaEnv {
   /**  本地缓存版本 */
