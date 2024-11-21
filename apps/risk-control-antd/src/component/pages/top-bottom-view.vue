d564d18cb7dca069e97d15ce49448d849b12a7dc
ZhangYantao <zhangyantao@rivergame.net>
2024-11-21 20:35:45 +0800

    feat: 添加topbottom组件

---
+++ apps/risk-control-antd/src/component/pages/top-bottom-view.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+// import { computed } from 'vue';
+
+defineOptions({
+  name: 'TopBottomView',
+});
+</script>
+
+<template>
+  <div class="base-container h-full w-full">
+    <ul
+      :style="{ 'list-style-type': 'none' }"
+      class="page-layout flex h-full w-full flex-col"
+    >
+      <li v-if="$slots.viewTopSlot" class="page-layout h-auto w-full flex-none">
+        <slot name="viewTopSlot"></slot>
+      </li>
+      <li
+        v-if="$slots.viewSecondSlot"
+        class="page-layout h-auto w-full flex-none"
+      >
+        <slot name="viewSecondSlot"></slot>
+      </li>
+      <li class="page-layout max-h-full w-full flex-1 overflow-y-hidden">
+        <slot name="viewBottomSlot"></slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
